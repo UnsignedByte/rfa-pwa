3154c4b7dc47104c4b90a042a6d4367e1c2265b1
UnsignedByte <edlam1889@gmail.com>
2020-06-06 00:05:49 -0700

    move some stuff

--- src/build.js
+++ src/build.js
@@ -2,10 +2,17 @@
 * @Author: UnsignedByte
 * @Date:   15:43:02, 05-Jun-2020
 * @Last Modified by:   UnsignedByte
-* @Last Modified time: 18:52:10, 05-Jun-2020
+* @Last Modified time: 20:42:08, 05-Jun-2020
 */
 
 import { promises as fs } from 'fs'
+
+/*
+ * Used to load and compile the curriculum folder into JSON format
+*/
+function load(params){
+	console.log(params);
+}
 
 const fname = 'params';
 const urls = {
@@ -14,8 +21,7 @@ const urls = {
 	js:new URL(`./build/${fname}.js`, import.meta.url)
 }
 fs.readFile(urls.main).then(async val => {
-		await fs.writeFile(urls.js, `const params = ${val}\nexport default params`)
-		import('./build/load.js').then(module => module.default())
+		load(JSON.parse(val));
 	})
 	.catch(err => {
 		// If the file doesn't exist, throw error after creating new file
