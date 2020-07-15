fd3865dc01b56c0c513d59d9bc926c10aaa232a4
UnsignedByte <edlam1889@gmail.com>
2020-07-14 18:25:09 -0700

    add jquery

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,20 @@
 * @Author: UnsignedByte
 * @Date:   16:10:21, 24-May-2020
 * @Last Modified by:   UnsignedByte
-* @Last Modified time: 16:14:41, 05-Jun-2020
+* @Last Modified time: 18:24:47, 14-Jul-2020
 */
+import resolve from '@rollup/plugin-node-resolve';
+
 export default {
   input: 'src/main.js',
   output: {
     file: 'build/curriculum.js',
     format: 'iife',
     name: 'Curriculum'
-  }
+  },
+  plugins: [
+  	resolve({
+			browser: true
+		})
+  ]
 };
---
+++ src/main.js
@@ -0,0 +1,10 @@
+/*
+* @Author: UnsignedByte
+* @Date:   16:01:48, 05-Jun-2020
+* @Last Modified by:   UnsignedByte
+* @Last Modified time: 18:21:18, 14-Jul-2020
+*/
+
+import * as jQuery from 'jquery';
+
+console.log($('<input/>'));
